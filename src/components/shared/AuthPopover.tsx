4ffb0a25232f5e5468aa8e7fa30b2105759cd36e
Maximus <mager5@yandex.ru>
2026-02-13 21:23:01 +0300

    🔧 Новый коммит: Исправление бага с прыгающей высотой меню профиля

--- src/components/shared/AuthPopover.tsx
+++ src/components/shared/AuthPopover.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useRef, useEffect } from 'react';
+import { useState, useRef, useEffect, Fragment } from 'react';
 import Link from 'next/link';
 import { User } from 'lucide-react';
 import { Transition } from '@headlessui/react';
@@ -94,6 +94,7 @@ export default function AuthPopover({
       </Link>
 
       <Transition
+        as={Fragment}
         show={isOpen}
         enter='transition ease-out duration-200'
         enterFrom='opacity-0 translate-y-1'
