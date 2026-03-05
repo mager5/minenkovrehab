6f002da012d0de7b176cd80d7a80c1d005b9da2e
Maximus <mager5@yandex.ru>
2026-03-05 13:30:39 +0300

    🔧 Fix video playback error and CSS preload warning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -196,7 +196,6 @@ export default function RootLayout({
           </main>
           <Footer />
           <CookieBanner />
-          {/* <HeyGenWidget /> */}
         </div>
       </body>
     </html>
