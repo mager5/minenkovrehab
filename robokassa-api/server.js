2ee01e1b6107685863e66a26bf4ac6e67bbc715b
iMax <imax@Mac-mini-iMax.local>
2026-06-23 11:11:07 +0300

    Add localhost:3002 to CORS allowed origins

--- robokassa-api/server.js
+++ robokassa-api/server.js
@@ -31,6 +31,8 @@ app.set('trust proxy', 1);
 const allowedOrigins = [
   'http://localhost:3000',
   'http://127.0.0.1:3000',
+  'http://localhost:3002',
+  'http://127.0.0.1:3002',
   process.env.FRONTEND_URL,
   'https://minenkovrehab.github.io',
   'https://minenkovrehab.ru',
